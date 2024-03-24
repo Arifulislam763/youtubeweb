41aaeb2f7c548e00c4c50e2b670a7081dd3d97ef
Arifulislam763 <121458055+Arifulislam763@users.noreply.github.com>
2024-03-24 23:53:23 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN","5749543675:AAGQj_Fc-DwohmH3sg7HO6c45T6KJ3CiTDw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","6926298518:AAGMfEe5ZAy_3LMfmzDllWVZIFOoJoMddRg")
 APP_ID = int(os.environ.get("API_ID","9273413"))
 API_HASH = os.environ.get("API_HASH","cc64f8b770f0960bb418768de4126336")
 HostName = os.environ.get("HostName","https://webtube.aryanvikash.com")
